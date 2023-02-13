6bab7d60b06f4c2e162a303c86b413dcca71471e
MORENO CÉSAR SANTOS SANTIAGO <22283401@cfp127aluno.local>
2023-02-13 11:05:43 -0300

    Finish papi

---
+++ app.js
@@ -0,0 +1,63 @@
+/*******************************************************************
+ * Objetivo: Criar um sistema de gerencie o cálculo de uma tabuada
+ * Data: 10/02/23
+ * Autor: Moreno
+ * Versão: 1.0
+ ******************************************************************/
+
+var readline = require('readline')
+var calculos = require('./MODULO/tabuada')
+
+var entradaDados = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+console.log('\n**********************MULTIPLICANDO**********************\n')
+entradaDados.question('Digite o número inicial da tabuada [ENTRE 2 E 100]: \n', function(numeroInicial) {
+
+    let tabuadaInicial = Number(numeroInicial.replace(',', '.'))
+
+    entradaDados.question('Digite o número final da tabuada [ENTRE 2 E 100]: \n', function(numeroFinal) {
+
+        let tabuadaFinal = Number(numeroFinal.replace(',', '.'))
+
+        if (tabuadaInicial > tabuadaFinal || tabuadaFinal < tabuadaInicial) {
+            console.log('ERRO: O número inicial não pode ser maior que o final')
+
+            entradaDados.close()
+
+        } else if (tabuadaInicial < 2 || tabuadaInicial > 100 || tabuadaFinal < 2 || tabuadaFinal > 100) {
+            console.log('ERRO: Digite apenas números entre 2 e 100')
+            entradaDados.close()
+
+        } else {
+
+            console.log('\n**********************MULTIPLICADOR**********************\n')
+            entradaDados.question('Digite o contador inicial da tabuada[ENTRE 1 E 50]: \n', function(valorContadorInicial) {
+
+                let contadorInicial = Number(valorContadorInicial.replace(',', '.'))
+
+                entradaDados.question('Digite o contador final da tabuada [ENTRE 1 E 50]: \n', function(valorContadorFinal) {
+
+                    let contadorFinal = Number(valorContadorFinal.replace(',', '.'))
+
+                    if (contadorInicial > contadorFinal || contadorFinal < contadorInicial) {
+                        console.log('ERRO: O número inicial não pode ser maior que o final')
+                        entradaDados.close()
+                    } else if (contadorInicial < 1 || contadorFinal > 50) {
+                        console.log('ERRO: Digite apenas números entre 1 e 50')
+                        entradaDados.close()
+
+                    } else {
+                        console.log(calculos.tabuada(tabuadaInicial, tabuadaFinal, contadorInicial, contadorFinal))
+                        entradaDados.close()
+                    }
+
+
+                })
+            })
+        }
+
+
+    })
+})
---
+++ modulo/tabuada.js
@@ -0,0 +1,36 @@
+/*******************************************************************
+ * Objetivo: Criar um sistema de gerencie  o cálculo de uma tabuada
+ * Data: 10/02/23
+ * Autor: Moreno
+ * Versão: 1.0
+ ******************************************************************/
+
+const tabuada = function(valorMultiplicandoInicio, valorMultiplicandoFinal, valorContadorInicio, valorContadorFinal) {
+
+    let resultado
+    let multiplicando = valorMultiplicandoInicio
+    let multiplicandoFinal = valorMultiplicandoFinal
+    let inicioContador
+    let fimContador = valorContadorFinal
+
+
+    while (multiplicando <= multiplicandoFinal) {
+        console.log(`\n*********** Tabuada do ${multiplicando} *********** \n`)
+
+        inicioContador = valorContadorInicio
+        while (inicioContador <= fimContador) {
+            resultado = multiplicando * inicioContador
+
+            console.log(`${multiplicando} x ${inicioContador} = ${resultado}`)
+            inicioContador++
+        }
+        multiplicando++
+
+    }
+
+
+}
+
+// console.log(tabuada(5, 10, 5, 10))
+
+module.exports = { tabuada }
